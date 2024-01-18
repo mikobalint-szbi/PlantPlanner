572af0cf77b76e183236efea2faca97e33af6c87
mikobalint-szbi <miko.balint.miklos@diak.szbi-pg.hu>
2024-01-18 08:28:26 +0100

    Initialize basic layuot

---
+++ src/routes/Planner/+page.svelte
@@ -0,0 +1,21 @@
+
+
+
+<section class="body">
+
+
+
+    <canvas class="field" />
+
+</section>
+
+<style>
+    .field{
+        border: 1px solid black;
+        width: 500px;
+        height: 650px;
+    }
+
+</style>
+
+
