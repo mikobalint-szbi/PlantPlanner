b6736a0e66d707e55a69b98fdc9553a12d9c520d
vbjhbj <119402788+vbjhbj@users.noreply.github.com>
2024-02-01 12:14:29 +0100

    export function "generator"

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,4 +1,4 @@
-function generator(amount:number, height:number, width:number){
+export function generator(amount:number, height:number, width:number){
 
     let plots =Array(amount).fill(Array(height).fill(new Array(width).fill(0)))
     return plots
