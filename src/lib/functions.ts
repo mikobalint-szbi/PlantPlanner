3fbae125ecc7364b847d315ce0ce397ef8f47dd2
vbjhbj <119402788+vbjhbj@users.noreply.github.com>
2024-02-15 11:12:49 +0100

    bug ficks

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -9,7 +9,7 @@ export interface PlantInput{
 
 export function generator(amount:number, height:number, width:number){
 
-    let plots =Array(amount).fill(new Array(height).fill(new Array(width).fill(0)))
+    let plots =Array(amount).fill(Array(height).fill(Array(width).fill(0)))
     return plots
 }
 
